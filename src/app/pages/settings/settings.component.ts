398f45c290dbda314cf8a236a38149675d384f74
Максим Богачёв <max.bogachew@yandex.ru>
2023-05-26 21:16:37 +0400

    Изменение страницы для настроек пользователей если администратор

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  DataTableHeaders: Array<string> = ["ID", "Логин", "ФИО", "Дата рождения", "Почта"];
+  DataTableContent: Array<any> = [
+    [0, "admin2", "Богачёв Максим Владиславович", "12/19/2002", "max.bogachew@yandex.ru"],
+    [1, "pochtiadmin", "Соловьев Алексей Сергеевич", "01/30/1988", "sas@solo-it.ru"]
+  ];
 
-  constructor() { }
+  constructor(public userService: UserService) {
+    userService.user$.subscribe(user=>{
+      this.DataTableContent.push([3, user.login, user.firstname+' '+user.secondname+' '+user.lastname, user.birthdate, user.mail])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  delete(user){
+    let userPosition = this.DataTableContent.findIndex(content=>content[0]==user[0]);
+    this.DataTableContent.splice(userPosition, 1);
   }
 
 }
