c48f63a80d1107d33ad0a2eb64e7c437af253bb7
Максим Богачёв <max.bogachew@yandex.ru>
2023-05-27 18:05:23 +0400

    Добавление роутингов

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,6 +6,9 @@ import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
 import { SettingsComponent } from "../../pages/settings/settings.component";
+import { CompaniesComponent } from "../../pages/companies/companies.component";
+import { ContactsComponent } from "../../pages/contacts/contacts.component";
+import { DealsComponent } from "../../pages/deals/deals.component";
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
@@ -13,5 +16,8 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'settings',       component: SettingsComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent }
+    { path: 'maps',           component: MapsComponent },
+    { path: 'companies',      component: CompaniesComponent },
+    { path: 'contacts',       component: ContactsComponent },
+    { path: 'deals',          component: DealsComponent }
 ];
