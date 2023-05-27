9a0ba74a617768e5ca991660a52e74ce766d2939
Максим Богачёв <max.bogachew@yandex.ru>
2023-05-27 21:06:28 +0400

    Изменение работы сервиса пользователя

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,32 +6,58 @@ import {BehaviorSubject, Observable} from "rxjs";
 })
 export class UserService {
 
-  private defaultUser = {
-    id: 3,
-    login: 'admin',
-    password: '12345678',
-    firstname: 'Богачёв',
-    secondname: 'Максим',
-    lastname: 'Владиславович',
-    work_phone: '89397052404',
-    personal_phone: '89023678122',
-    mail: 'max.bogachew@yandex.ru',
-    birthdate: '12/19/2002'
-  };
-  private _userSubject:BehaviorSubject<any> = new BehaviorSubject<any>(this.defaultUser);
-
+  private defaultUsers = [
+    {
+      id: 0,
+      login: 'admin',
+      password: 'Maks2343',
+      firstname: 'Богачёв',
+      secondname: 'Максим',
+      lastname: 'Владиславович',
+      work_phone: '89397052404',
+      personal_phone: '89023678122',
+      mail: 'max.bogachew@yandex.ru',
+      birthdate: '12/19/2002'
+    },
+    {
+      id: 1,
+      login: 'pochtiAdmin',
+      password: '12345678',
+      firstname: 'Соловьев',
+      secondname: 'Алексей',
+      lastname: 'Сергеевич',
+      work_phone: '89023678122',
+      personal_phone: '89397052404',
+      mail: 'sas@solo-it.ru',
+      birthdate: '12/19/2002'
+    },
+    {
+      id: 2,
+      login: 'manager',
+      password: '12345678',
+      firstname: 'Юсупов',
+      secondname: 'Султан',
+      lastname: 'Какой-тович',
+      work_phone: '89397052404',
+      personal_phone: '89023678122',
+      mail: 'm.usupov@solo-it.ru',
+      birthdate: '12/19/2002'
+    }
+  ];
+  private _userSubject:BehaviorSubject<any> = new BehaviorSubject<any>(this.defaultUsers[0]);
+  private _usersSubject:BehaviorSubject<any> = new BehaviorSubject<any>(this.defaultUsers);
   public user$:Observable<any> = this._userSubject.asObservable();
 
   constructor() { }
 
-  public getAuthStatus(){
+  public getAuthStatus():boolean{
     if(this._userSubject.value!=null){
       return true;
     }
     return false;
   }
 
-  public getUserType(){
+  public getUserType():Object{
     const random = Math.floor(Math.random()*2);
     switch (random) {
       case 0:
@@ -41,5 +67,20 @@ export class UserService {
         return {'name': 'manager', 'id': 1};
         break;
     }
+  }
+
+  public auth(login:string, password:string):boolean{
+    let authUser = false;
+    this.defaultUsers.forEach(user=>{
+      if(user.login==login && user.password==password){
+        this._userSubject.next(user);
+        authUser = true;
+      }
+    });
+    return authUser;
+  }
+
+  public deAuth(){
+    this._userSubject.next(null);
   }
 }
