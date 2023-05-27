2d794e3f1e79e2ae5daba30e31816ed67bf72332
Максим Богачёв <max.bogachew@yandex.ru>
2023-05-27 17:57:35 +0400

    Изменения

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from "../../services/user.service";
-import { ModalDismissReasons, NgbDatepickerModule, NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDismissReasons, NgbDatepickerModule, NgbDatepicker, NgbModal, NgbActiveModal, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import {element} from "protractor";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-settings',
@@ -12,13 +13,35 @@ export class SettingsComponent implements OnInit {
   closeResult:any = '';
   DataTableHeaders: Array<string> = ["ID", "Логин", "ФИО", "Дата рождения", "Почта"];
   DataTableContent: Array<any> = [
-    [0, "admin2", "Богачёв Максим Владиславович", "12/19/2002", "max.bogachew@yandex.ru"],
-    [1, "pochtiadmin", "Соловьев Алексей Сергеевич", "01/30/1988", "sas@solo-it.ru"]
+    {
+      id: 0,
+      login: 'admin2',
+      password: '12345678',
+      firstname: 'Богачёв',
+      secondname: 'Максим',
+      lastname: 'Владиславович',
+      work_phone: '89397052404',
+      personal_phone: '89023678122',
+      mail: 'max.bogachew@yandex.ru',
+      birthdate: '12/19/2002'
+    },
+    {
+      id: 1,
+      login: 'pochtiadmin',
+      password: '12345678',
+      firstname: 'Богачёв',
+      secondname: 'Максим',
+      lastname: 'Владиславович',
+      work_phone: '89397052404',
+      personal_phone: '89023678122',
+      mail: 'max.bogachew@yandex.ru',
+      birthdate: '12/19/2002'
+    }
   ];
 
   constructor(public userService: UserService, private modalService: NgbModal) {
     userService.user$.subscribe(user=>{
-      this.DataTableContent.push([3, user.login, user.firstname+' '+user.secondname+' '+user.lastname, user.birthdate, user.mail])
+      this.DataTableContent.push(user);
     })
   }
 
@@ -30,16 +53,9 @@ export class SettingsComponent implements OnInit {
     this.DataTableContent.splice(userPosition, 1);
   }
 
-  edit(){
-    // this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-    //   (result) => {
-    //     this.closeResult = `Closed with: ${result}`;
-    //   },
-    //   (reason) => {
-    //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    //   },
-    // );
-    const modalRef = this.modalService.open(NgbdModalContent);
+  edit(user){
+    const modalRef = this.modalService.open(SettingsDialogContentComponent);
+    modalRef.componentInstance.user = user;
 
     setTimeout(()=>{
       const element = document.getElementsByClassName('modal-backdrop fade show');
@@ -47,40 +63,71 @@ export class SettingsComponent implements OnInit {
       element.item(0).remove();
     }, 100);
 
-
-  }
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } /*else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    }*/ else {
-      return `with: ${reason}`;
-    }
+    modalRef.result.then(result=>{
+      // console.log(result);
+      let users = this.DataTableContent;
+      let i = 0;
+      users.forEach(user=>{
+        if(user.id==result.id){
+          this.DataTableContent[i] = result;
+        }
+        i+=1;
+      });
+    });
   }
 
 }
 
 @Component({
-  selector: 'ngbd-modal-content',
+  selector: 'settings-dialog-content',
   standalone: true,
-  template: `
-		<div class="modal-header">
-			<h4 class="modal-title">Hi there!</h4>
-			<button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
-		</div>
-		<div class="modal-body">
-			<p>Hello, {{ name }}!</p>
-		</div>
-		<div class="modal-footer">
-			<button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
-		</div>
-	`,
+  templateUrl: './settings-dialog-content.component.html',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
-export class NgbdModalContent {
-  // @Input() name;
-  name = 'hello';
+export class SettingsDialogContentComponent {
+  @Input() user:any;
+  userSettingsGroup: FormGroup = new FormGroup<any>({
+    login: new FormControl(''),
+    password: new FormControl(''),
+    firstname: new FormControl(''),
+    secondname: new FormControl(''),
+    lastname: new FormControl(''),
+    birthdate: new FormControl(''),
+    workPhone: new FormControl(''),
+    personalPhone: new FormControl(''),
+    mail: new FormControl('')
+  });
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(public activeModal: NgbActiveModal) {
+    setTimeout(()=>{
+      this.userSettingsGroup.get('login').setValue(this.user.login);
+      this.userSettingsGroup.get('password').setValue(this.user.password);
+      this.userSettingsGroup.get('firstname').setValue(this.user.firstname);
+      this.userSettingsGroup.get('secondname').setValue(this.user.secondname);
+      this.userSettingsGroup.get('lastname').setValue(this.user.lastname);
+      this.userSettingsGroup.get('birthdate').setValue(this.user.birthdate);
+      this.userSettingsGroup.get('workPhone').setValue(this.user.work_phone);
+      this.userSettingsGroup.get('personalPhone').setValue(this.user.personal_phone);
+      this.userSettingsGroup.get('mail').setValue(this.user.mail);
+    }, 100);
+  }
+
+  save(){
+    const user = {
+      id: this.user.id,
+      login: this.userSettingsGroup.get('login').value,
+      password: this.userSettingsGroup.get('password').value,
+      firstname: this.userSettingsGroup.get('firstname').value,
+      secondname: this.userSettingsGroup.get('secondname').value,
+      lastname: this.userSettingsGroup.get('lastname').value,
+      birthdate: this.userSettingsGroup.get('birthdate').value,
+      work_phone: this.userSettingsGroup.get('workPhone').value,
+      personal_phone: this.userSettingsGroup.get('personalPhone').value,
+      mail: this.userSettingsGroup.get('mail').value
+    };
+    return user;
+  }
 }
 
