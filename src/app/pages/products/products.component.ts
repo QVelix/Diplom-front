5cc5f1ad918fc8871b5993ccfb6fa6d8c98e0116
Максим Богачёв <max.bogachew@yandex.ru>
2023-05-28 13:22:00 +0400

    Добавление всплывающего окна для редактирования

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {SettingsDialogContentComponent} from "../settings/settings.component";
 
 @Component({
   selector: 'app-products',
@@ -11,7 +14,7 @@ export class ProductsComponent implements OnInit {
     {id:6,name: 'БУС', price: 19000}, {id:7,name: 'Б24', price: 50000}, {id:8,name: 'Б24М', price: 9000},
     {id:9,name: 'БУС', price: 19000}, {id:10,name: 'Б24', price: 50000}, {id:11,name: 'Б24М', price: 9000}]
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
@@ -20,4 +23,51 @@ export class ProductsComponent implements OnInit {
     this.products.splice(this.products.findIndex(prod=>prod.id==product.id),1);
   }
 
+  edit(product){
+    const modalRef = this.modalService.open(ProductsDialogContentComponent);
+    modalRef.componentInstance.product = product;
+
+    setTimeout(()=>{
+      const element = document.getElementsByClassName('modal-backdrop fade show');
+      element.item(0).remove();
+    }, 100);
+
+    modalRef.result.then(result=>{
+      let userPosition = this.products.findIndex(content=>content.id==result.id);
+      this.products[userPosition] = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'products-dialog-content',
+  standalone: true,
+  templateUrl: './products-dialog-content.component.html',
+  imports: [
+    ReactiveFormsModule
+  ]
+})
+export class ProductsDialogContentComponent {
+  @Input() product:any;
+  productsGroup: FormGroup = new FormGroup<any>({
+    name: new FormControl(''),
+    price: new FormControl(''),
+  });
+
+  constructor(public activeModal: NgbActiveModal) {
+    setTimeout(()=>{
+      this.productsGroup.get('name').setValue(this.product.name);
+      this.productsGroup.get('price').setValue(this.product.price);
+    }, 100);
+  }
+
+  save(){
+    const product = {
+      id: this.product.id,
+      name: this.productsGroup.get('name').value,
+      price: this.productsGroup.get('price').value,
+    };
+    return product;
+  }
 }
