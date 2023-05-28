93c72dea06380d4e46b36e20e64300ea504685f2
Максим Богачёв <max.bogachew@yandex.ru>
2023-05-28 13:10:13 +0400

    Написание продуктов

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  products: any[] = [{id:0,name: 'БУС', price: 19000}, {id:1,name: 'Б24', price: 50000}, {id:2,name: 'Б24М', price: 9000},
+    {id:3,name: 'БУС', price: 19000}, {id:4,name: 'Б24', price: 50000}, {id:5,name: 'Б24М', price: 9000},
+    {id:6,name: 'БУС', price: 19000}, {id:7,name: 'Б24', price: 50000}, {id:8,name: 'Б24М', price: 9000},
+    {id:9,name: 'БУС', price: 19000}, {id:10,name: 'Б24', price: 50000}, {id:11,name: 'Б24М', price: 9000}]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  delete(product){
+    this.products.splice(this.products.findIndex(prod=>prod.id==product.id),1);
   }
 
 }
