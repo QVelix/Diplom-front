f25d9fa653a02720422414687fb8689c06d1a306
Максим Богачёв <m.bogachev@solo-it.ru>
2023-06-01 09:10:07 +0400

    Написание сендлера

--- src/app/services/sendler.service.ts
+++ src/app/services/sendler.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SendlerService {
+  private url = 'localhost:5054';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public get(url:string, value:any=null){
+    if(value==null){
+      return this.http.get(this.url+url).pipe(map(response=>{return response}));
+    }else{
+      return this.http.get(this.url+url, value).pipe(map(response=>{return response}));
+    }
+  }
+
+  public post(url:string, value:any){
+    return this.http.post(this.url+url, value, {withCredentials:true}).pipe(map(response=>{return response}));
+  }
+
+  public put(url:string, value:any):void{
+    this.http.put(this.url+url, value, {withCredentials:true});
+  }
+
+  public delete(url:string, value:any):void{
+    this.http.delete(this.url+url, value);
+  }
 }
