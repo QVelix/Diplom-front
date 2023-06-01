a9683bb67d6abd0d6e20e0a2b205bc36b7161b83
Максим Богачёв <m.bogachev@solo-it.ru>
2023-06-01 09:09:49 +0400

    Использование sendler'а и модели пользователя

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,55 +1,26 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
+import {SendlerService} from "./sendler.service";
+import {User} from "../models/User";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private defaultUsers = [
-    {
-      id: 0,
-      login: 'admin',
-      password: 'Maks2343',
-      firstname: 'Богачёв',
-      secondname: 'Максим',
-      lastname: 'Владиславович',
-      work_phone: '89397052404',
-      personal_phone: '89023678122',
-      mail: 'max.bogachew@yandex.ru',
-      birthdate: '12/19/2002'
-    },
-    {
-      id: 1,
-      login: 'pochtiAdmin',
-      password: '12345678',
-      firstname: 'Соловьев',
-      secondname: 'Алексей',
-      lastname: 'Сергеевич',
-      work_phone: '89023678122',
-      personal_phone: '89397052404',
-      mail: 'sas@solo-it.ru',
-      birthdate: '12/19/2002'
-    },
-    {
-      id: 2,
-      login: 'manager',
-      password: '12345678',
-      firstname: 'Юсупов',
-      secondname: 'Султан',
-      lastname: 'Какой-тович',
-      work_phone: '89397052404',
-      personal_phone: '89023678122',
-      mail: 'm.usupov@solo-it.ru',
-      birthdate: '12/19/2002'
-    }
-  ];
-  private _userSubject:BehaviorSubject<any> = new BehaviorSubject<any>(this.defaultUsers[0]);
-  private _usersSubject:BehaviorSubject<any> = new BehaviorSubject<any>(this.defaultUsers);
+  private _userSubject:BehaviorSubject<any> = new BehaviorSubject<Array<User>>(null);
+  private _usersSubject:BehaviorSubject<any> = new BehaviorSubject<User>(null);
   public user$:Observable<any> = this._userSubject.asObservable();
   public users$:Observable<any> = this._usersSubject.asObservable();
 
-  constructor() { }
+  constructor(private sendlerService: SendlerService) {
+    sendlerService.get('/api/UsersController').subscribe((users:Array<User>)=>{
+      users.forEach(user=>{
+        this._usersSubject.value.next(user);
+      })
+    });
+  }
+
 
   public getAuthStatus():boolean{
     if(this._userSubject.value!=null){
@@ -58,22 +29,46 @@ export class UserService {
     return false;
   }
 
-  public getUserType():Object{
-    const random = Math.floor(Math.random()*2);
-    switch (random) {
-      case 0:
-        return {'name': 'admin', 'id': 0};
-        break;
-      case 1:
-        return {'name': 'manager', 'id': 1};
-        break;
-    }
+  public getUserType(){
+    let findedType = null;
+    this.sendlerService.get('/api/UserTypeContoller').subscribe((types:Array<{ID:number, Name:string}>)=>{
+      types.forEach(type=>{
+        if(this._userSubject.value.ID == type.ID){
+          findedType = type;
+        }
+      })
+    });
+    return findedType;
+  }
+
+  public searchUser(search:string){
+    let findedUser;
+    this._usersSubject.value.forEach((user:User)=>{
+      if(user.Login.includes(search)||user.First_name.includes(search)||user.Second_name.includes(search)||(user.First_name+" "+user.Second_name).includes(search)){
+        findedUser = user;
+      }
+    });
+    return findedUser;
+  }
+
+  public addUser(user){
+    this.sendlerService.put('/api/UsersController', user);
+    this._usersSubject.value.next(user);
+  }
+
+  public deleteUser(userId){
+    this.sendlerService.delete('/api/UsersController', userId);
+    this._usersSubject.value.splice(this._usersSubject.value.findIndex(user=>user.ID==userId),1);
+  }
+
+  public changeUser(user) {
+    this._usersSubject.value[this._usersSubject.value.findIndex(u => u.ID == user.ID)] = user;
   }
 
   public auth(login:string, password:string):boolean{
     let authUser = false;
-    this.defaultUsers.forEach(user=>{
-      if(user.login==login && user.password==password){
+    this._usersSubject.value.forEach(user=>{
+      if(user.Login==login && user.Password==password){
         this._userSubject.next(user);
         authUser = true;
       }
@@ -82,6 +77,6 @@ export class UserService {
   }
 
   public deAuth(){
-    this._userSubject.next(null);
+    this._userSubject.value.splice(0);
   }
 }
